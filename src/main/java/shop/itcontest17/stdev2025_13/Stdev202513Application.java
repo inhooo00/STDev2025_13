2f56c6165c10a7994ab6f6643eb70b03f9109f9f
inhooo00 <chldlsgh0987@naver.com>
2025-04-18 22:44:59 +0900

    first commit

---
+++ src/main/java/shop/itcontest17/stdev2025_13/Stdev202513Application.java
@@ -0,0 +1,13 @@
+package shop.itcontest17.stdev2025_13;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Stdev202513Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Stdev202513Application.class, args);
+    }
+
+}
---
+++ src/test/java/shop/itcontest17/stdev2025_13/Stdev202513ApplicationTests.java
@@ -0,0 +1,13 @@
+package shop.itcontest17.stdev2025_13;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Stdev202513ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
