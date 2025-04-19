fb072f1135a6af4e5068ba5529fe710405e433f3
inhooo00 <chldlsgh0987@naver.com>
2025-04-19 19:46:42 +0900

    Feature(#3): 워드 클라우드 구현을 위한 감정 리스트 반환

--- src/main/java/shop/itcontest17/stdev2025_13/member/api/MemberController.java
+++ src/main/java/shop/itcontest17/stdev2025_13/member/api/MemberController.java
@@ -1,11 +1,27 @@
 package shop.itcontest17.stdev2025_13.member.api;
 
+import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import shop.itcontest17.stdev2025_13.global.annotation.CurrentUserEmail;
+import shop.itcontest17.stdev2025_13.global.template.RspTemplate;
+import shop.itcontest17.stdev2025_13.member.api.dto.response.EmotionCountResDto;
+import shop.itcontest17.stdev2025_13.member.application.MemberService;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("api/members")
 public class MemberController {
+
+    private final MemberService memberService;
+
+    @GetMapping("/top-emotions")
+    public RspTemplate<List<EmotionCountResDto>> getTopEmotions(@CurrentUserEmail String email) {
+        return new RspTemplate<>(HttpStatus.OK,
+                "탑 5개 감정반환 성공",
+                memberService.getTop5EmotionsByEmail(email));
+    }
 }
